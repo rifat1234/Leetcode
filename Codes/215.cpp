bb32c8cad6e909bf6c3192d3c7194ad5f724978c
Rifat Monzur <rifatmonzur@gmail.com>
2025-01-04 12:00:41 +0000

    Create 215.cpp

---
+++ Codes/215.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int,vector<int>,greater<int>> q;
+        for(auto num:nums){
+            q.push(num);
+            if(q.size()>k)q.pop();
+        }
+
+        return q.top();
+    }
+};
