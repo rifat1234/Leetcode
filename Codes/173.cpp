edf14fee32f14ed774c85eba4cf9f0048bfbbc64
Rifat Monzur <rifatmonzur@gmail.com>
2025-01-17 17:55:09 +0000

    Create 173.cpp

---
+++ Codes/173.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator {
+private:
+    queue<int> q;
+public:
+    BSTIterator(TreeNode* root) {
+        build(root);
+    }
+    
+    void build(TreeNode* root){
+        if(root==nullptr)return;
+        build(root->left);
+        q.push(root->val);
+        build(root->right);
+    }
+    
+    int next() {
+        int val = q.front();
+        q.pop();
+        return val;
+    }
+    
+    bool hasNext() {
+        return !q.empty();
+    }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
